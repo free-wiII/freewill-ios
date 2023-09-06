de7894fa7ef8d6cd23267468af2e382702ee7f2a
Wallaby <avocado34.131@gmail.com>
2023-09-06 15:56:39 +0900

    [Feat] 커스텀 탭바 추가

---
+++ freewill/Resources/ColorSet.swift
@@ -0,0 +1,25 @@
+//
+//  ColorSet.swift
+//  freewill
+//
+//  Created by 이승기 on 2023/09/06.
+//
+
+import SwiftUI
+
+extension Color {
+  
+  static let fwBlack = Color(uiColor: R.color.black()!)
+  static let fwGreen = Color(uiColor: R.color.green()!)
+  static let fwWhite = Color(uiColor: R.color.white()!)
+  
+  
+  // MARK: - Gray
+  
+  static let fwGray = Color(uiColor: R.color.gray()!)
+  static let fwGray20 = Color(uiColor: R.color.gray20()!)
+  static let fwGray30 = Color(uiColor: R.color.gray30()!)
+  static let fwGray40 = Color(uiColor: R.color.gray40()!)
+  static let fwGray60 = Color(uiColor: R.color.gray60()!)
+  static let fwGray70 = Color(uiColor: R.color.gray70()!)
+}
