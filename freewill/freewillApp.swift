41725809270f860bfe760db0cdaa0277aac57256
Wallaby <avocado34.131@gmail.com>
2023-09-06 14:53:53 +0900

    Initial Commit

---
+++ freewill/freewillApp.swift
@@ -0,0 +1,17 @@
+//
+//  freewillApp.swift
+//  freewill
+//
+//  Created by 이승기 on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct freewillApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
