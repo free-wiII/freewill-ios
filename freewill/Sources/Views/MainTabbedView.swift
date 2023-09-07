5fc1a2d8b2bc4a09d992aea93292745019a4e869
Wallaby <avocado34.131@gmail.com>
2023-09-07 17:37:01 +0900

    [Feat] MainTabbedView 구현

---
+++ freewill/Sources/Views/MainTabbedView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabbedView.swift
+//  freewill
+//
+//  Created by 이승기 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct MainTabbedView: View {
+  
+  // MARK: - Properties
+  
+  @State private var selectedTab: TabItem = .main
+  
+  
+  // MARK: - Views
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      TabView(selection: $selectedTab) {
+        Text("main")
+          .tag(TabItem.main)
+        
+        Text("bookmark")
+          .tag(TabItem.bookmark)
+        
+        Text("myPage")
+          .tag(TabItem.myPage)
+      }
+      
+      TabBar(selectedTab: $selectedTab)
+    }
+  }
+}
+
+
+// MARK: - Preview
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    MainTabbedView()
+  }
+}
--- freewill/Sources/freewillApp.swift
+++ freewill/Sources/freewillApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct freewillApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      MainTabbedView()
     }
   }
 }
