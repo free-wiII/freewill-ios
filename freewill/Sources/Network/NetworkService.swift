122edb19be8f4d07d5a5ac8b02030fcceae25b0c
Wallaby <avocado34.131@gmail.com>
2023-09-16 00:49:25 +0900

    [Feat] 클린아키텍쳐 구성
    - 메인 피드 리스트 목 데이터 추가

---
+++ freewill/Sources/Network/NetworkService.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkService.swift
+//  freewill
+//
+//  Created by 이승기 on 2023/09/15.
+//
+
+import Foundation
+import Moya
+import Combine
+import CombineMoya
+
+enum NetworkServiceType {
+  case server
+  case stub
+}
+
+final class NetworkService<Target: TargetType> {
+  
+  // MARK: - Properties
+  
+  private let provider: MoyaProvider<Target>
+  private let decoder = JSONDecoder()
+
+  
+  // MARK: - Initializers
+  
+  init(_ serviceType: NetworkServiceType) {
+    switch serviceType {
+    case .server:
+      provider = MoyaProvider<Target>()
+    case .stub:
+      provider = MoyaProvider<Target>(stubClosure: MoyaProvider.delayedStub(1.0))
+    }
+  }
+  
+  
+  // MARK: - Publics
+  
+  public func request<T: Decodable>(_ target: Target, type: T.Type) -> AnyPublisher<T, Error> {
+    provider
+      .requestPublisher(target)
+      .map(\.data)
+      .decode(type: T.self, decoder: decoder)
+      .eraseToAnyPublisher()
+  }
+}
